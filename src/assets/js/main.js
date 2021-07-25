f0fa7c261937882eaded3cfa9bd571b4df485923
Tristan Hampton <tristanhampton@icloud.cmo>
2021-07-25 13:24:37 -0600

    Cleaned up nav scss

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,16 +1,37 @@
 //--- Menu
-let hamburger = document.querySelector('.hamburger');
-let nav = document.querySelector('nav');
-let header = document.querySelector('header')
+const menuToggle = document.querySelector('.header__menu-toggle');
+const menuToggleClose = document.querySelector('.site__nav button.close');
+const navLinks = document.querySelectorAll('.site__nav a');
+const nav = document.querySelector('.site__nav');
+const header = document.querySelector('.site__header')
 
-const handleMenu = () => {
-    nav.classList.toggle('active');
-    hamburger.classList.toggle('transform')
+const openMenu = () => {
+    nav.classList.add('active');
 }
 
+const closeMenu = () => {
+    nav.classList.remove('active');
+}
+
+menuToggle.addEventListener('click', function() {
+    openMenu();
+});
+
+menuToggleClose.addEventListener('click', function() {
+    closeMenu();
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => closeMenu());
+});
+
 header.addEventListener('click', (evt) => {
-    if (evt.target.classList.contains('hamburger') || evt.target.nodeName == 'A'){
-        handleMenu();
+    if (evt.target == menuToggle){
+        openMenu();
+    }
+
+    if(evt.target == menuToggleClose || evt.target.nodeName == 'A') {
+        closeMenu();
     }
 });
 
